70c8f14f4027daf5aaefc202a637389418602e5a
IvanW5X <ivanwong169@gmail.com>
2025-09-22 23:57:25 -0700

    Initial app setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Description: Main file for audio production tool
+ * 
+ * Date: 2025-09-22
+ * 
+ * Author: Ivan Wong
+ * 
+ **********************************************************/
+
+#include <QApplication>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}

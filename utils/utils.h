9f858289a8f47db43590351e85a22f0c6896c8a9
IvanW5X <ivanwong169@gmail.com>
2025-09-25 19:43:59 -0700

    Add utils file, function TBD

---
+++ utils/utils.h
@@ -0,0 +1,10 @@
+/**********************************************************
+ * Description: Helper functiond for the audio production tool
+ * 
+ * Date: 2025-09-24
+ * 
+ * Author: Ivan Wong
+ * 
+ **********************************************************/
+
+#pragma once

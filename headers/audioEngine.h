131974d5086766e2eb808b9e644e3855458ff532
IvanW5X <ivanwong169@gmail.com>
2025-10-03 13:04:33 -0700

    Remove template from insertTagData()

--- headers/audioEngine.h
+++ headers/audioEngine.h
@@ -52,74 +52,10 @@ class AudioEngine : public QObject
         // Helper functions
         inline void sendResponse(AudioCommand::PacketPtr &packet, const QVariant &Payload);
         AudioData::MetaDataMap_T getAudioMetaData(const QString &FileName);
+        bool insertTagData(const AudioData::Keys Key, const TagLib::Tag *Tag, AudioData::MetaDataMap_T &destMap);
         bool insertPropertyData(const AudioData::Keys Key, const TagLib::AudioProperties *Tag, AudioData::MetaDataMap_T &destMap);
-
 
         // Disable copy constructor and assignment operator overload
         AudioEngine(const AudioEngine &) = delete;
         AudioEngine &operator=(const AudioEngine &) = delete;
-
-        // Template functions
-        // Adds the data tag associated with the key to the map, inserts NA if no tag is found
-        template<typename T>
-        bool insertTagData(const AudioData::Keys Key, const TagLib::Tag *Tag, AudioData::MetaDataMap_T &destMap, const T &Converter)
-        {
-            bool success = true;
-
-            if (!tag)
-            {
-                ErrorHandle::instance().handleError
-                (
-                    Error::ReadingFileFailed,
-                    QString("Failed to get tag data.")
-                );
-                return !success;
-            }
-            TagLib::String buffer;
-            QVariant data;
-
-            switch (Key)
-            {
-                case AudioData::Title:
-                {
-                    buffer = tag->title(); 
-                    break;
-                }
-                case AudioData::Album:{
-                    buffer = tag->album();
-                    break;
-                }
-                case AudioData::Artist:
-                { 
-                    buffer = tag->artist();
-                    break;
-                }
-                case AudioData::Genre:
-                {
-                    buffer = tag->genre();
-                    break;
-                }
-                default:
-                {
-                    Error::instance().handleError
-                    (
-                        Error::ReadingFileFailed,
-                        QString("File has no audio tage for %1.").arg(Key)
-                    );
-                    return !success;
-                }
-            }
-            if (buffer.isEmpty())
-            {
-                data = QVariant::fromValue(QString("NA"));
-                sucess = false;
-            }
-            else
-            {
-                data = QVariant::fromValue(QString::fromStdWString(buffer.toCWString()));
-            }
-            destMap.insert(Key, data);
-
-            return success;
-        }
 };
